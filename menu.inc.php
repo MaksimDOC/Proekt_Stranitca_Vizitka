e23a9bb9870c8e1fda1dc84268e664de6c74c6f1
MaksimDOC <93469721+MaksimDOC@users.noreply.github.com>
2021-11-06 02:16:13 +0300

    Delete menu.inc.php

--- menu.inc.php
+++
@@ -1,8 +0,0 @@
-<nav>
-                <ul>
-                    <li><a href="index.php">Главная</a></li>
-                    <li><a href="raboty.html">Проекты</a></li>
-                    <li><a href="silka.html">Ресурсы</a></li>
-                    <li><a href="kontakt.html">Контакты</a></li>
-                </ul>    
-            </nav>
